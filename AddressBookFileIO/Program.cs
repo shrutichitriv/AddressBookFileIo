8adffa190e58080da93a01d541ad02cb19631a8b
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-02 16:31:38 +0530

    [Shruti] Perform Addressbook File IO Operation

---
+++ AddressBookFileIO/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace AddressBookFileIO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Addressbook File IO Program");
+        }
+    }
+}
