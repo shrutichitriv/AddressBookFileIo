ebeec28e2ed09bd3e1848974259054515ea54f71
shrutichitriv <shrutichitriv199620@gmail.com>
2023-02-02 17:01:57 +0530

    [Shruti] Ability to read or write the Addressbook using File IO Operation

---
+++ AddressBookFileIO/MyFileIOOperation/FileIO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookFileIO.MyFileIOOperation
+{
+    internal class FileIO
+    {
+        public static void WriteRecordsInFile(string path, PersonInput person)
+        {
+
+            if (File.Exists(path))
+            {
+                StreamWriter sw = File.AppendText(path);
+                sw.WriteLine("First Name : " + person.fName);
+                sw.WriteLine("Last Name : " + person.lName);
+                sw.WriteLine("Address : " + person.address);
+                sw.WriteLine("City : " + person.city);
+                sw.WriteLine("State : " + person.state);
+                sw.WriteLine("Email : " + person.email);
+                sw.WriteLine("Zip code : " + person.zip);
+                sw.WriteLine();
+                sw.Close();
+                Console.WriteLine("Data added successfully in file");
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+        }
+        public static void ReadRecordsFromFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                Console.WriteLine(File.ReadAllText(path));
+            }
+            else
+            {
+                Console.WriteLine("File Not Found");
+            }
+        }
+    }
+}
--- AddressBookFileIO/Program.cs
+++ AddressBookFileIO/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using AddressBookFileIO.MyFileIOOperation;
+
 namespace AddressBookFileIO
 {
     internal class Program
@@ -6,6 +7,31 @@ namespace AddressBookFileIO
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Addressbook File IO Program");
+
+            string path = @"C:\Users\HP\source\RFP_244\AddressBookFileIo\AddressBookFileIO\MyFiles\PersonContact.txt";
+
+            PersonInput input = new PersonInput();
+            Console.WriteLine("Enter your First Name : ");
+            input.fName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name : ");
+            input.lName = Console.ReadLine();
+            Console.WriteLine("Enter your Address : ");
+            input.address = Console.ReadLine();
+            Console.WriteLine("Enter your City Name : ");
+            input.city = Console.ReadLine();
+            Console.WriteLine("Enter your State Name : ");
+            input.state = Console.ReadLine();
+            Console.WriteLine("Enter your Zip Code : ");
+            input.zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Phone Number : ");
+            input.phoneNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your Email Address: ");
+            input.email = Console.ReadLine();
+
+            FileIO.WriteRecordsInFile(path, input);
+            Console.WriteLine("Records present in file are : ");
+            FileIO.ReadRecordsFromFile(path);
+            Console.ReadLine();
         }
     }
 }
